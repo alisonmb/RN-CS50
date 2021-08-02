b8bfec529c6a4a4d99dd6d7446043566f3f2f8e9
alison borba <alison.borba@paladiotecnologia.com.br>
2021-08-01 22:37:13 -0300

    Separate files

--- App.js
+++ App.js
@@ -1,84 +1,23 @@
-import React from 'react';
-import {View, Button, Text, StyleSheet} from 'react-native'
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import CustomCount from './Count.js'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <CustomCount count={0} />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  appContainer: {
+  container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  count: {
-    fontSize: 48,
-  }
-})
-
-class Counter extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      count: 0,
-    }
-  }
-  
-  componentDidMount() {
-    this.interval = setInterval(this.inc, 1000)
-  }
-  
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-  
-  inc = () => {
-    console.log('increment!')
-    this.setState(prevState => ({
-      count: prevState.count + 1,
-    }))
-  }
-
-  render() {
-    return (
-      <Text style={styles.count}>{this.state.count} </Text>
-    )
-  }
-}
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showCounter: true,
-    }
-  }
-  
-  toggleCounter = () => this.setState(prevState => ({
-    showCounter: !prevState.showCounter,
-  }))
-
-  // this was the render() code originally written in lecture
-  render() {
-    if (this.state.showCounter) {
-      return (
-        <View style={styles.appContainer}>
-          <Button title="toggle" onPress={this.toggleCounter} />
-          <Counter />
-        </View>
-      )
-    } else {
-      return (
-        <View style={styles.appContainer}>
-          <Button title="toggle" onPress={this.toggleCounter} />
-        </View>
-      )
-    }
-  }
-
-  // this is a more concise version with the same functionality
-  render() {
-    return (
-      <View style={styles.appContainer}>
-        <Button title="toggle" onPress={this.toggleCounter} />
-        {this.state.showCounter && <Counter />}
-      </View>
-    )
-  }
-}
+});
